4e4825e281db916208f265c614096e109df38595
Ognjen <ognjen.popovic@ajnagaming.com>
2026-04-01 10:17:17 +0200

    Footer done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
 
 export default defineConfig({
   site: 'https://www.vibrumguitars.com',
-  integrations: [sitemap()],
+  integrations: [sitemap(), icon()],
   output: 'static',
 });
